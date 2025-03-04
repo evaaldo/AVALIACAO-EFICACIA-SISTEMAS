5ffd5295e8fc5b1ac3688ed769cc346c64d5f26e
evaldo <evaaldormj@gmail.com>
2025-03-04 13:36:59 -0300

    Feat: Add open left sidebar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FormInputComponent } from './form-input/form-input.component';
 import { FormActionComponent } from './form-action/form-action.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FormInputComponent, FormActionComponent],
+  imports: [CommonModule, HeaderComponent, FormInputComponent, FormActionComponent, LeftSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
