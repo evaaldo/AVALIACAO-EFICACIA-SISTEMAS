d063e7612192c0db6f64b480f41e5f2d1d601392
evaldo <evaaldormj@gmail.com>
2025-03-04 13:52:11 -0300

    Feat: Conditionate the sidebar

--- src/app/left-sidebar/left-sidebar.component.ts
+++ src/app/left-sidebar/left-sidebar.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-left-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.css'
 })
 export class LeftSidebarComponent {
-
+  isClosed: boolean = true;
 }
