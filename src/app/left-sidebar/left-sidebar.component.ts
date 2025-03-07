042db31cd069c1618415395aebfef6dde9eb268f
evaldo <evaaldormj@gmail.com>
2025-03-06 22:49:50 -0300

    Feat: Add router links to sidebar

--- src/app/left-sidebar/left-sidebar.component.ts
+++ src/app/left-sidebar/left-sidebar.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-left-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.css'
 })
