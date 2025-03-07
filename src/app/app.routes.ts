db55cca163c3faf20df27f5c98071bc2d6321a81
evaldo <evaaldormj@gmail.com>
2025-03-06 22:47:44 -0300

    Feat: Add dashboard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
     path: "",
     component: FormComponent
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent
   }
 ];
